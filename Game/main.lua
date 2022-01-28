e8047c5f77404805d1e5b9ddf217afacdd1130b3
chexInCollege <97613898+chexInCollege@users.noreply.github.com>
2022-01-27 20:05:55 -0500

    added patronizing function

--- Game/main.lua
+++ Game/main.lua
@@ -1,6 +1,9 @@
 i = 1
 
+function love.draw()
+    love.graphics.print("i've been waiting for " .. i .. " seconds")
+end
+
 function love.update(dt)
     i = i + dt
-    love.graphics.print("i've been waiting for " .. i .. " seconds")
 end
